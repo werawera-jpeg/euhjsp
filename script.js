75a9de3bddf76358c7b4d083021189a05512b319
werawera-jpeg <loranasl17@gmail.com>
2025-10-02 23:53:18 +0300

    1

---
+++ script.js
@@ -0,0 +1,46 @@
+const username = document.getElementById('user');
+const expectedPassword = '19'; 
+const imageUrl = 'images/gateau.jfif'; 
+const finalText = "'And give thanks for everything to God the Father in the name of our Lord Jesus Christ.' - Eph 5:20";
+
+const passwd = document.getElementById('pass');
+const error = document.getElementById('err');
+const goBtn = document.getElementById('go');
+const loginCard = document.getElementById('loginCard');
+const surpriseCard = document.getElementById('surpriseCard');
+const surpriseImg = document.getElementById('surpriseImg');
+const surpriseTitle = document.getElementById('surpriseTitle');
+const surpriseText = document.getElementById('surpriseText');
+
+surpriseText.textContent = finalText;
+
+function showError(msg){
+  error.textContent = msg;
+  error.classList.remove('hidden');
+//   setTimeout(()=>error.classList.add('hidden'), 3000);
+}
+
+async function check(p){
+  if( p === expectedPassword) return true;
+  return false;
+}
+
+goBtn.addEventListener('click', async () => {;
+    surpriseImg.setAttribute("src", imageUrl);
+    const u = username.value;
+    const finalTitle = 'happy birthday ' + u + ' ! 🥳';
+    surpriseTitle.textContent = finalTitle;
+
+    const p = passwd.value;
+    if(!u || !p || u===" ") { showError('remplis les champs gros'); return; }
+    const ok = await check(p);
+    if(ok){
+        loginCard.classList.add('hidden');
+        surpriseCard.classList.remove('hidden');
+    } else {
+        showError('mdp incorrect, donne le vrai age');
+    }
+});
+
+// Enter key submits
+// [userEl,passwd].forEach(el => el.addEventListener('keydown', (e)=>{ if(e.key==='Enter'){ goBtn.click(); } }));
